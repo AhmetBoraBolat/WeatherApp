309200af8c1f6164b91378cab5fe7e68e50e8516
Ahmet Bora Bolat <borabolat2015@gmail.com>
2023-07-30 23:14:29 +0300

    Update Utils.kt

--- app/src/main/java/com/ahmetborabolat/weatherapp/Utils.kt
+++ app/src/main/java/com/ahmetborabolat/weatherapp/Utils.kt
@@ -4,7 +4,9 @@ class Utils {
 
     companion object{
         const val BASE_URL : String = "https://api.openweathermap.org/data/2.5/"
-        const val API_KEY : String = "86479c17866b10cbe591e6f527094f2c"
+
+
+        const val API_KEY : String = "" // https://home.openweathermap.org/api_keys get your api key from here
         const val PERMISSION_REQUEST_CODE = 123
     }
 
